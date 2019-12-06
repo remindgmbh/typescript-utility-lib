221ae1dcab5f67d09ef26812d850e0df92a09a3e
Hauke Schulz <h.schulz@remind.de>
2019-12-06 12:28:37 +0100

    Fixed ts/js interoperability

--- .mocharc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    require: [
-        'ts-node/register',
-        'jsdom-global/register'
-    ],
-    extension: [ 'ts' ],
-    recursive: true,
-    reporter: 'mocha-junit-reporter'
-}
